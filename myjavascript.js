ee43a0f27a31e73728164e13fbe35c915724cbf0
stampejd <stampejd@gmail.com>
2016-11-12 16:47:51 -0500

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,10 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			I am the first element in the carousel.
+		</div>
+		<div class="item">
+			I am the second element in the carousel.
+		</div>
+	</div>
+</div>
